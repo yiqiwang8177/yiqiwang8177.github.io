cb98e2ffd54f3a0b6b15ecca26d51cf54414ba68
Yiqi Wang <69227993+berniewang8177@users.noreply.github.com>
2023-12-07 11:44:40 -0500

    some popup
    some popup

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-function showPopup() {
-document.getElementById('popup').style.display = 'block';
-}
+// function showPopup() {
+// document.getElementById('popup').style.display = 'block';
+// }
 
-function closePopup() {
-document.getElementById('popup').style.display = 'none';
-}
+// function closePopup() {
+// document.getElementById('popup').style.display = 'none';
+// }
