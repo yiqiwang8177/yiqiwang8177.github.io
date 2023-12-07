67978db699af9087e825ef41da2ef04f30b92e2a
Yiqi Wang <69227993+berniewang8177@users.noreply.github.com>
2023-12-07 10:48:10 -0500

    add latest script
    add latest script

--- main.js
+++ main.js
@@ -1,24 +1,7 @@
-// var mobileMenuBtn = document.querySelector("#mobile-menu-btn");
-// var mobileMenu = document.querySelector(".mobile-menu");
+function showPopup() {
+document.getElementById('popup').style.display = 'block';
+}
 
-// mobileMenuBtn.addEventListener("click", () => {
-//   if (mobileMenu.style.display === "none") {
-//     mobileMenu.style.display = "flex";
-//     mobileMenuBtn.innerHTML = "Close";
-//   } 
-//   else {
-//     mobileMenu.style.display = "none";
-//     mobileMenuBtn.innerHTML = "Menu";
-//   }
-// });
-
-// $ = function(id) {
-//   return document.getElementById(id);
-// }
-
-// var show = function(id) {
-// 	$(id).style.display ='block';
-// }
-// var hide = function(id) {
-// 	$(id).style.display ='none';
-// }
+function closePopup() {
+document.getElementById('popup').style.display = 'none';
+}
